fd45942b5a4d75204ab4c7f81c09e3939d3ebdf5
Naga <nagachiangdev@gmail.com>
2024-12-04 17:52:50 +0800

    Add base path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/stockheap',
+  assetPrefix: '/stockheap',
 };
 
 export default nextConfig;
