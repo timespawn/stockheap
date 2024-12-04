da9dbcd9f6f455150c81a502d85389afc3688a4c
Naga <nagachiangdev@gmail.com>
2024-12-04 18:33:16 +0800

    Let github action insert base path

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/stockheap',
-  assetPrefix: '/stockheap',
 };
 
 export default nextConfig;
